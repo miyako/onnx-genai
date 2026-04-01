fd6b2ae7f5849e028e314e5b3ffb8d20146d5882
Keisuke Miyako <keisuke.miyako@4D.com>
2026-04-01 18:50:30 +0900

    comment out session_options.DisableMemPattern();

--- cpp/onnx-genai/main.cpp
+++ cpp/onnx-genai/main.cpp
@@ -2663,7 +2663,7 @@ int main(int argc, OPTARG_T argv[]) {
                     {
                         Ort::SessionOptions probe_opts;
                         probe_opts.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_DISABLE_ALL);
-                        probe_opts.DisableMemPattern();
+//                        probe_opts.DisableMemPattern();
                         probe_opts.DisableCpuMemArena();
                         Ort::Session probe(*embeddings_env, embedding_model_path.c_str(), probe_opts);
 
@@ -2756,7 +2756,7 @@ int main(int argc, OPTARG_T argv[]) {
                     session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
 
                     session_options.AddConfigEntry("session.intra_op.allow_spinning", "0");
-                    session_options.DisableMemPattern();
+//                    session_options.DisableMemPattern();
                     session_options.DisableCpuMemArena();
                     
                     Ort::ThrowOnError(RegisterCustomOps((OrtSessionOptions*)session_options, OrtGetApiBase()));
@@ -2854,7 +2854,7 @@ int main(int argc, OPTARG_T argv[]) {
                     session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
 
                     session_options.AddConfigEntry("session.intra_op.allow_spinning", "0");
-                    session_options.DisableMemPattern();
+//                    session_options.DisableMemPattern();
                     session_options.DisableCpuMemArena();
                     
                     Ort::ThrowOnError(RegisterCustomOps((OrtSessionOptions*)session_options, OrtGetApiBase()));
@@ -2934,7 +2934,7 @@ int main(int argc, OPTARG_T argv[]) {
                 sopts.SetGraphOptimizationLevel(
                     GraphOptimizationLevel::ORT_ENABLE_ALL);
                 sopts.AddConfigEntry("session.intra_op.allow_spinning", "0");
-                sopts.DisableMemPattern();
+//                sopts.DisableMemPattern();
                 sopts.DisableCpuMemArena();
 #ifdef WIN32
                 tts_session = std::make_unique<Ort::Session>(*tts_env, tts_model_path_u16.c_str(), sopts);
